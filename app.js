a82a1729d96afc1638a46fac423bbf0be8c52788
Valentris <cvalentris@gmail.com>
2024-10-16 14:37:30 +0800

    Update Port

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use('/api/todos', todoRoutes);
 app.use('/api/tasks', taskRoutes);
 
 app.listen(8000, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
